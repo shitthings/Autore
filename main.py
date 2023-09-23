a5009d5ab5d43d8d0b9d3445762c6dbffb931f1b
shitthings <143640498+shitthings@users.noreply.github.com>
2023-09-23 23:41:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,45 +4,52 @@ import random
 import string
 import csv
 
-# Function to generate a random URL and check its status
+# Function to generate random URL and check its status
 def generate_random_url():
     base_url = "https://autoreg.site/status/"
     random_string = ''.join(random.choices(string.hexdigits, k=32))
     url = base_url + random_string
     response = requests.get(url)
-    return url, response.status_code == 200
+    if response.status_code == 200:
+        return url
+    else:
+        return None
 
 # Streamlit app
 st.title("Random URL Generator")
 
-attempts_count = 0
-successful_urls = []
-
 if st.button("Start Generating"):
+    success_attempts = []
+    failed_attempts = []
+    attempts_count = 0
+
     while True:
+        url = generate_random_url()
         attempts_count += 1
-        url, is_successful = generate_random_url()
         st.text(f"Attempt {attempts_count}: {url}")
 
-        if is_successful:
-            successful_urls.append(url)
+        if url:
+            success_attempts.append(url)
+        else:
+            failed_attempts.append(url)
 
-        if not st.button("Generate Another URL"):
+        # Check if the user wants to stop
+        if not st.button("Stop"):
             break
 
-# Display download button for successful URLs as CSV
-if successful_urls:
-    st.header("Successful URLs")
-    st.write(successful_urls)
-    
-    if st.button("Download Successful URLs as CSV"):
+# Download successful attempts as CSV
+if st.button("Download Successful Attempts CSV"):
+    if success_attempts:
         with st.spinner("Generating CSV..."):
-            with open('successful_urls.csv', 'w', newline='') as file:
+            with open('successful_attempts.csv', 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(["URL"])
-                writer.writerows([[url] for url in successful_urls])
+                writer.writerows([[url] for url in success_attempts])
         st.success("CSV file generated successfully!")
-        st.markdown(f"Download [successful_urls.csv](successful_urls.csv)")
+        st.markdown(f"Download [successful_attempts.csv](successful_attempts.csv)")
 
+# Display statistics
 st.header("Statistics")
-st.text(f"Total Successful Attempts: {attempts_count}")
+st.text(f"Total Attempts: {attempts_count}")
+st.text(f"Successful Attempts: {len(success_attempts)}")
+st.text(f"Failed Attempts: {len(failed_attempts)}")
